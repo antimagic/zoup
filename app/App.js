535ba1bb40aa0e28df0ae621999e51278873fbf1
James Firkins <arceihn@gmail.com>
2019-02-27 23:05:43 +1030

    :art: Organised styles into files per section

--- app/App.js
+++ app/App.js
@@ -77,39 +77,37 @@ class App extends Component {
       <div className="zoup">
         <Filters onChange={this.onFilterChange} />
 
-        <section className="content__wrapper">
-            <div className="columns">
-              <div className="column is-one-third-tablet results">
-                <h3 className="results__heading">{this.getResultsHeading()}</h3>
-                <ul className="results__listing">{this.formattedResults()}</ul>
-              </div>
+        <section className="content__wrapper columns">
+          <div className="column is-one-third-tablet results">
+            <h3 className="results__heading">{this.getResultsHeading()}</h3>
+            <ul className="results__listing">{this.formattedResults()}</ul>
+          </div>
 
-              <div className="column selected__wrapper">
-                {selected !== null && (
-                  <div className="selected">
-                    <div className="columns">
-                      <div className="column">
-                        <img className="selected__image" src={selected.featured_image}></img>
-                      </div>
+          {selected !== null && (
+          <div className="column selected__wrapper">
+            <div className="selected">
+              <div className="columns">
+                <div className="column">
+                  <img className="selected__image" src={selected.featured_image}></img>
+                </div>
 
-                      <div className="column">
-                        <h2>{selected.name}</h2>
-                        <address>{selected.location.address}</address>
-                        <ul className="selected__summary">
-                          <li className={selected.has_table_booking ? 'yes' : 'no'}>{selected.has_table_booking ? 'Table bookings available' : 'No bookings'}</li>
-                          <li className={selected.is_delivering_now ? 'yes' : 'no'}>{selected.is_delivering_now ? 'Delivery available' : 'No delivery available'}</li>
-                        </ul>
-                        <div className="selected__cuisines">
-                          <h3>Cuisines</h3>
-                          <span>{selected.cuisines}</span>
-                        </div>
-                        <a href={selected.url} target="_blank" className="selected__link">{selected.name} at Zomato</a>
-                      </div>
-                    </div>
+                <div className="column selected__summary">
+                  <h2>{selected.name}</h2>
+                  <address>{selected.location.address}</address>
+                  <ul className="selected__features">
+                    <li className={selected.has_table_booking ? 'yes' : 'no'}>{selected.has_table_booking ? 'Table bookings available' : 'No bookings'}</li>
+                    <li className={selected.is_delivering_now ? 'yes' : 'no'}>{selected.is_delivering_now ? 'Delivery available' : 'No delivery available'}</li>
+                  </ul>
+                  <div className="selected__cuisines">
+                    <h3>Cuisines</h3>
+                    <span>{selected.cuisines}</span>
                   </div>
-                )}
+                  <a href={selected.url} target="_blank" className="selected__link">{selected.name} at Zomato</a>
+                </div>
               </div>
             </div>
+          </div>
+          )}
         </section>
       </div>
     )
