cdd4ab86f3a4d84066d122169339e3af3daf664b
James Firkins <arceihn@gmail.com>
2019-02-28 03:37:36 +1030

    :art: Reorganised asset file structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.react('app/index.js', 'dist/').sass('app/scss/app.scss', 'dist/')
+mix.react('app/index.js', 'dist/').sass('assets/scss/app.scss', 'dist/')
    .options({
      processCssUrls: false,
    });
